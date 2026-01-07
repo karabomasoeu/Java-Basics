a4419588d92226476a5f13ff7f7c3b0702d07846
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-01-07 22:31:20 +0200

    Update simple_calculator.java

--- simple_calculator.java
+++ simple_calculator.java
@@ -8,4 +8,9 @@ public class SimpleCalculator {
         
         System.out.println("Enter operator (+, -, *, /):");
         char operator = sc.next().charAt(0);
-       
+        
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        
+        double result = 0;
+        boolean validOperation = true;
